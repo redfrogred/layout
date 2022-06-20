cca38c02a59f40f56f7b5f3a802b815243584c0a
Matt <matt.garvin.416@gmail.com>
2022-06-20 15:39:52 -0400

    added some ui

--- lib/pages/Page1.dart
+++ lib/pages/Page1.dart
@@ -5,9 +5,14 @@ import 'package:page_transition/page_transition.dart';
 import '../pages/_all.dart';
 import '../widgets/bottom_nav.dart';
 
-class Page1 extends StatelessWidget {
+class Page1 extends StatefulWidget {
   const Page1 ({Key? key}) : super(key: key);
 
+  @override
+  State<Page1> createState() => _Page1State();
+}
+
+class _Page1State extends State<Page1> {
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
@@ -19,7 +24,8 @@ class Page1 extends StatelessWidget {
         resizeToAvoidBottomInset: false, // see BLADE-74
         appBar: AppBar(
           title: const Text('Settings'),
-          automaticallyImplyLeading: false,
+          automaticallyImplyLeading: true,
+          centerTitle: true,
         ),
         body: Container(
           decoration: const BoxDecoration(
@@ -31,25 +37,170 @@ class Page1 extends StatelessWidget {
             ),
             color: Color(0xFFE6E6E6),
           ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Expanded(
-                child: Center(
-                  child:Text('vanilla_page.dart\n(denies back button)'),
-                  ),
-              ),
-              Expanded(
-                child: Center(
-                  child: ElevatedButton(
-                    child: Text('< back'),
-                    onPressed: () {
-                      Navigator.pop(context);
+          child: Container(
+            width: double.infinity,
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(8,8,8,8),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  
+                  
+                  
+                  
+                  
+                  // start of BASICS
+                  InkWell(
+                    onTap: () {
+                      Utils.log('ink me!');
                     },
+                    child: Container(
+                      color: Color(0xFF888888),
+                      child: Row(
+                        children: [
+                          Expanded(
+                            flex: 8,
+                            child: Container(
+                              color: Color(0xFF888888),
+                              child: Padding(
+                                padding: const EdgeInsets.fromLTRB(14,8,8,8),
+                                child: Text(
+                                  'Basics', style: TextStyle( fontSize: 18, color: Colors.white)),
+                              ))),
+                          Expanded(
+                            flex: 1,
+                            child: Container(
+                              alignment: Alignment.center,
+                              color: Colors.grey,
+                              child: Padding(
+                                padding: const EdgeInsets.fromLTRB(8,8,8,8),
+                                child: Text(
+                                  '>', style: TextStyle( fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white )),
+                              ))),
+                                      
+                        ],
+                      ),
+                    ),
                   ),
-                ),
+                  // end of BASICS
+
+
+
+                  const Divider(height: 8),
+
+
+
+                  // start of REMOTE UNLOCK
+                  Material(
+                    color: const Color(0xcff888888),
+                    child: InkWell(
+                      splashColor: Color(0xcffCCCCCC),
+                      onTap: () {
+                        Utils.log('ink me!');
+                      },
+                      child: Container(
+                        //color: Color(0xFF888888),
+                        child: Row(
+                          children: [
+                            Expanded(
+                              flex: 8,
+                              child: Container(
+                                //color: Colors.transparent,
+                                child: const Padding(
+                                  padding: EdgeInsets.fromLTRB(14,8,8,8),
+                                  child: Text(
+                                    'Remote Unlock', style: TextStyle( fontSize: 18, color: Colors.white)),
+                                ))),
+                            Expanded(
+                              flex: 1,
+                              child: Container(
+                                alignment: Alignment.center,
+                                //color: Colors.grey,
+                                child: const Padding(
+                                  padding: EdgeInsets.fromLTRB(8,8,8,8),
+                                  child: Text(
+                                    '>', style: TextStyle( fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white )),
+                                ),
+                                decoration: const BoxDecoration(
+                                  // color: Color(0xcffCCCCCC),
+                                  border: Border(  
+                                    left: BorderSide (
+                                      color: Color(0xcff777777),
+                                      width: 2,
+                                    )
+                                  )
+                                ),                                
+                              ),
+                            ),          
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  // end of REMOTE UNLOCK
+
+
+
+
+                  const Divider(height: 8),
+
+                  
+
+
+                  // start of FIRMWARE
+                  InkWell(
+                    onTap: () {
+                      Utils.log('ink me!');
+                    },
+                    child: Container(
+                      color: Color(0xFF888888),
+                      child: Row(
+                        children: [
+                          Expanded(
+                            flex: 8,
+                            child: Container(
+                              color: Color(0xFF888888),
+                              child: Padding(
+                                padding: const EdgeInsets.fromLTRB(14,8,8,8),
+                                child: Text(
+                                  'Firmware Update', style: TextStyle( fontSize: 18, color: Colors.white)),
+                              ))),
+                          Expanded(
+                            flex: 1,
+                            child: Container(
+                              alignment: Alignment.center,
+                              color: Colors.grey,
+                              child: Padding(
+                                padding: const EdgeInsets.fromLTRB(8,8,8,8),
+                                child: Text(
+                                  '>', style: TextStyle( fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white )),
+                              ))),
+                                      
+                        ],
+                      ),
+                    ),
+                  ),
+                  // end of FIRMWARE
+
+
+
+                  Padding( 
+                    padding: EdgeInsets.fromLTRB(10,50,10,10),
+                    child: Align(
+                      alignment: Alignment.topLeft,
+                      child: Text('Here is the cool thing...\nThe "Remote Unlock" has an InkWell effect!\n(Click it to see the magic of the ripple!)', 
+                        style: TextStyle(
+                          fontSize: 18,
+                        ),
+                      )
+                    ),
+                  ),
+
+
+
+                ]
               ),
-            ]
+            ),
           ),
         ),
         bottomNavigationBar: const BottomNav(),
